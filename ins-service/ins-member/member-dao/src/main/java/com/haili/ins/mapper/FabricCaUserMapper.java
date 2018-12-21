03a7c494de36922bb5ea50006d62b1ed38876a1b
top3372 <top3372@163.com>
2018-12-21 12:46:13 +0800

    添加auth鉴权服务模块

--- ins-service/ins-member/member-dao/src/main/java/com/haili/ins/mapper/FabricCaUserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.haili.ins.mapper;
-
-import com.haili.ins.common.database.mapper.BaseMapper;
-import com.haili.ins.model.FabricCaUser;
-
-public interface FabricCaUserMapper extends BaseMapper<FabricCaUser> {
-}
